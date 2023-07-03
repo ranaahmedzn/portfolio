1ec5d62dec3cd26261e74942bbe1af849f8f1396
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-07-03 16:38:41 +0600

    added contact info in the contact me section and made some changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
             <div className="h-[calc(100vh-80px)] max-w-7xl px-10 mx-auto flex justify-between items-center">
                 <div className="w-3/5">
                     <p className="text-3xl font-bold text-purple-500">HEY!</p>
+                    {/* <h3 className='text-6xl font-bold text-gray-100'>I&apos;m Rana Sheikh</h3> */}
                     <TypeAnimation
                         sequence={[
                             // Same substring at the start will only be typed once, initially
@@ -23,10 +24,10 @@ const Hero = () => {
                             1000,
                         ]}
                         speed={50}
-                        style={{ fontSize: '48px', fontWeight: 'bold', color: "white" }}
+                        style={{ fontSize: '50px', fontWeight: 'bold', color: "white" }}
                         repeat={Infinity}
                     />
-                    <p className="mt-4 mb-6 w-full md:w-3/4 text-gray-300">I am a passionate front-end web application developer. I mainly work with MERN stack. I have been working in this field for 1 years.</p>
+                    <p className="mt-4 mb-6 w-full md:w-5/6 text-gray-300">Welcome to my captivating portfolio website! I am a passionate front-end web application developer with expertise in building exceptional websites using the powerful MERN stack. With 1 year of experience in the field, I bring creativity, skill, and a strong dedication to delivering remarkable digital experiences. Explore my portfolio and witness the magic of my creations!</p>
                     <div>
                         <ScrollLink to="projects" smooth={true} duration={500} offset={-80}>
                             <button className="px-7 py-3 mr-5 relative inline-flex items-center justify-center font-medium text-white bg-gray-800 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group">
@@ -34,12 +35,12 @@ const Hero = () => {
                                 <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
                                 <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
                                 <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-                                <span className="relative">View Work</span>
+                                <span className="relative">View Works</span>
                             </button>
                         </ScrollLink>
 
                         <ScrollLink to="contact" smooth={true} duration={500} offset={-80}>
-                            <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-purple-500 transition duration-300 ease-out border-2 border-purple-500 rounded-lg group">
+                            <button className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-medium text-purple-500 transition duration-300 ease-out border-2 border-purple-500 hover:border-transparent rounded-lg group">
                                 <span className="absolute inset-0 flex gap-2 items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-gradient-to-br from-blue-500 to-purple-600 group group-hover:translate-x-0 ease">
                                     <span>Hire Me</span>
                                     <FaArrowRight />
